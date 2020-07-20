d77d7b25de648ae86cb52492bd7ae044a7af8ecd
chancald <chandler.calderon@gmail.com>
2020-07-20 21:36:48 +0000

    [UPD] added production config

--- personal-website/settings.py
+++ personal-website/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y_jg9w8ju#a2kzvj)*(3+h%cdacdd=)s9jjzbltg6i0r==%&s^'
-
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['34.74.25.248', '127.0.0.1']
 
