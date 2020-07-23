55dbfd04f56086ede8dc01bb5239a61c87a559be
chancald <chandler.calderon@gmail.com>
2020-07-23 17:05:30 -0300

    [FIX] urls, views

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,4 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('projects', views.projects, name='projects'),
-    path('contact', views.contact, name='contact'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,19 +2,10 @@ from django.shortcuts import render, HttpResponse
 from home.models import Contact
 
 def home(request):
-    return render(request, 'home/home.html')
-
-def about(request):
-    return render(request, 'home/about.html')
-
-def projects(request):
-    return render(request, 'home/projects.html')
-
-def contact(request):
     if request.method=="POST":
         name = request.POST['name']
         email = request.POST['email']
         desc = request.POST['desc']
         contact = Contact(name=name, email=email, desc=desc)
         contact.save()
-    return render(request, 'home/contact.html')
+    return render(request, 'home/home.html')
