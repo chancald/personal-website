0ffd99d6d3bc3c99843b4675fd634b3a3634de74
Chandler Calderon <chancald@Chandlers-MacBook-Air.local>
2021-01-08 16:26:47 -0400

    [UPD] settings.py and requirements.txt

--- personal-website/settings.py
+++ personal-website/settings.py
@@ -119,8 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = ''
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
 ]
